fccfc678c41a63e18ff7fc723b16e76011d3ba47
BastD29 <bastiendaniel9@gmail.com>
2024-01-23 18:58:18 +0100

    fixed type bugs for deploy

--- src/api/postsApi.tsx
+++ src/api/postsApi.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // Not needed until optimistic UI updates example
-const delay = () => new Promise((res) => setTimeout(() => res(), 1800));
+const delay = () => new Promise<void>((res) => setTimeout(() => res(), 1800));
 
 const postsApi = axios.create({
   baseURL: "http://localhost:3500",
@@ -9,7 +9,7 @@ const postsApi = axios.create({
 
 export const postsUrlEndpoint = "/posts";
 
-export const getPostsByUserId = async (url, userId) => {
+export const getPostsByUserId = async (url: string, userId: number) => {
   await delay();
   const response = await postsApi.get(`${url}?userId=${userId}`);
   return response.data;
--- src/api/usersApi.tsx
+++ src/api/usersApi.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // Not needed until optimistic UI updates example
-const delay = () => new Promise((res) => setTimeout(() => res(), 1800));
+const delay = () => new Promise<void>((res) => setTimeout(() => res(), 1800));
 
 const usersApi = axios.create({
   baseURL: "http://localhost:3500",
@@ -15,7 +15,7 @@ export const getUsers = async () => {
   return response.data;
 };
 
-export const getUserById = async (url, userId) => {
+export const getUserById = async (url: string, userId: number) => {
   await delay();
   const response = await usersApi.get(`${url}/${userId}`);
   return response.data;
